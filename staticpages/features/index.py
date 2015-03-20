1944bdb1a9968fe58dfaedeb16460c4294535e13
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-03-20 18:01:15 +0000

    cambio en index

--- staticpages/features/index.py
+++ staticpages/features/index.py
@@ -1,18 +1,20 @@
-from lettuce import *
+import urllib2
 from lxml import html
-from django.test.client import Client
 from nose.tools import assert_equals
+from lettuce import world, before, step
+from lettuce.django import django_url
+from django.test.client import Client
 
 @before.all
 def set_client():
 	world.browser = Client()
-
+	
 @step(r'I access the url "(.*)"')
 def given_i_access_the_url(step, url):
 	url = django_url(url)
 	raw = urllib2.urlopen(url).read()
 	world.dom = html.fromstring(raw)
 	
-@step(r'I see the header "(.*)"')
-def then_i_see_header(step, text):
-	world.element = world.dom.cssselect("h1")[0]
+@step(r'I see the title "(.*)"')
+def see_title(step, text):
+	assert text == world.dom.find(".//title").text
